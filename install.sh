1b2bb3b3c9adbbd0753d7fc8044ed5b171e647a4
ldz <2872611588@qq.com>
2022-05-03 15:55:31 +0800

    调整install.sh下载插件位置

--- install.sh
+++ install.sh
@@ -4,15 +4,15 @@ home_path=`pwd`
 rm ~/.vimrc
 ln -s ${home_path}/vimrc ~/.vimrc
 
-# 下载vim插件
-for (( i=0; i<10; i++ ));
-do
-bash ./vim.git
-done
-
 sudo apt install -y nodejs npm
 sudo npm install -g n
 sudo n stable
 sudo apt install -y python3-pip
 sudo apt install -y python3-venv
+
+# 下载vim插件
+for (( i=0; i<10; i++ ));
+do
+bash ./vim.git
+done
 

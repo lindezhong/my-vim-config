b8980137b99d98079def4b22c352e484576e9190
ldz <2872611588@qq.com>
2024-07-25 22:25:30 +0800

    禁用更新提醒

--- dconf/dconf.sh
+++ dconf/dconf.sh
@@ -4,5 +4,8 @@ dconf write /org/gnome/shell/extensions/dash-to-dock/dock-fixed false
 dconf write /org/gnome/shell/extensions/dash-to-dock/intellihide false
 dconf write /org/gnome/shell/extensions/dash-to-dock/autohide true
 
+# 禁用更新提醒
+dconf write /apps/update-notifier/auto_launch false
+
 # ubuntu屏幕不自动旋转
 gsettings set org.gnome.settings-daemon.peripherals.touchscreen orientation-lock true
